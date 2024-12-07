4c20b9b81ac85811388cacbb44cbeb74fead8efa
Deborah Wang <dqw@sfu.ca>
2024-12-07 00:50:21 -0800

    add font testing on page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,17 @@
-import Image from "next/image";
-
-// const geistSans = localFont({
-//   src: "./fonts/GeistVF.woff",
-//   variable: "--font-geist-sans",
-//   weight: "100 900",
-// });
-// const geistMono = localFont({
-//   src: "./fonts/GeistMonoVF.woff",
-//   variable: "--font-geist-mono",
-//   weight: "100 900",
-// }); ${geistSans.variable} ${geistMono.variable}
-
 export default function Home() {
   return (
     <div
       className={`grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
     >
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        Something is coming ...
+        <div className="flex flex-col gap-4 text-2xl">
+          <h2 className="font-planar font-bold">GT Planar Bold</h2>
+          <h2 className="font-planar font-black">GT Planar Black</h2>
+          <h2 className="font-apparat font-normal">KMR Apparat Regular</h2>
+          <h2 className="font-apparat font-medium">KMR Apparat Medium</h2>
+          <h2 className="font-mono font-normal">CoFo Sans Regular</h2>
+          <h2 className="font-mono font-medium">CoFo Sans Medium</h2>
+        </div>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
       </footer>
