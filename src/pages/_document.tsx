94a5a02484f6f87024280414f99ce451184c088b
Deborah Wang <dqw@sfu.ca>
2024-12-09 22:04:03 -0800

    udpate scaling ith correct queries

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,66 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import {Html, Head, Main, NextScript} from "next/document";
+import Navbar from "@/components/navbar/navbar";
 
 export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body className="antialiased">
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+    return (
+        <Html lang="en">
+            <Head>
+                <link
+                    rel="icon"
+                    type="image/png"
+                    sizes="16x16"
+                    href="/favicons/favicon_16x16.png"/>
+                <link
+                    rel="icon"
+                    type="image/png"
+                    sizes="32x32"
+                    href="/favicons/favicon_32x32.png"/>
+
+                <meta name="msapplication-TileColor" content="#EA530E"/>
+                <meta name="msapplication-TileImage" content="/favicons/favicon_144x144.png"/>
+
+                <link
+                    rel="apple-touch-icon"
+                    sizes="180x180"
+                    href="/favicons/favicon_180x180.png"/>
+
+                <link
+                    rel="apple-touch-icon-precomposed"
+                    sizes="180x180"
+                    href="/favicons/favicon_180x180.png"/>
+
+                <link
+                    rel="icon"
+                    type="image/png"
+                    sizes="192x192"
+                    href="/favicons/favicon_192x192.png"/>
+
+                <link
+                    rel="icon"
+                    type="image/png"
+                    sizes="194x194"
+                    href="/favicons/favicon_194x194.png"/>
+
+                <link
+                    rel="mask-icon"
+                    href="/favicons/favicon_svg.svg"
+                    color="#EA530E"/>
+
+                <link
+                    rel="icon"
+                    sizes="32x32 48x48"
+                    type="image/x-icon"
+                    href="/favicons/favicon.ico"/>
+
+                <link
+                    rel="shortcut icon"
+                    type="image/x-icon"
+                    href="/favicons/favicon.ico"/>
+            </Head>
+            <body className={"antialiased"}>
+                <NextScript/>
+                <Main/>
+            </body>
+        </Html>
+    );
 }
