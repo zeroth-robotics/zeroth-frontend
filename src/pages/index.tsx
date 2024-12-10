7f3f83a2d845922f4e36c08103c86075672f8cc7
Deborah Wang <dqw@sfu.ca>
2024-12-09 17:23:40 -0800

    update colours

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,15 +3,33 @@ export default function Home() {
         <div
             className={`grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 
       gap-16 sm:p-20 `}
-            >
+        >
             <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
                 <div className="flex flex-col gap-4 text-2xl">
-                    <h2 className="font-planar font-bold">GT Planar Bold</h2>
-                    <h2 className="font-planar font-normal">GT Planar Regular</h2>
-                    <h2 className="font-apparat font-bold">KMR Apparat Bold</h2>
-                    <h2 className="font-apparat font-medium">KMR Apparat Medium</h2>
-                    <h2 className="font-mono font-normal">CoFo Sans Regular</h2>
-                    <h2 className="font-mono font-medium">CoFo Sans Medium</h2>
+                    <svg width="50" height="50" xmlns="http://www.w3.org/2000/svg">
+                        <rect width="50" height="50" x="10" y="10" className={"fill-accent-aubergine"}/>
+                    </svg>
+                    Aubergine
+                    <svg width="50" height="50" xmlns="http://www.w3.org/2000/svg">
+                        <rect width="50" height="50" x="10" y="10" className={"fill-accent-dragonfruit"}/>
+                    </svg>
+                    Dragonfruit
+                    <svg width="50" height="50" xmlns="http://www.w3.org/2000/svg">
+                        <rect width="50" height="50" x="10" y="10" className={"fill-accent-apple"}/>
+                    </svg>
+                    Apple
+                    <svg width="50" height="50" xmlns="http://www.w3.org/2000/svg">
+                        <rect width="50" height="50" x="10" y="10" className={"fill-accent-blood-orange"}/>
+                    </svg>
+                    Blood Orange
+                    <svg width="50" height="50" xmlns="http://www.w3.org/2000/svg">
+                        <rect width="50" height="50" x="10" y="10" className={"fill-accent-tangerine"}/>
+                    </svg>
+                    Tangerine
+                    <svg width="50" height="50" xmlns="http://www.w3.org/2000/svg">
+                        <rect width="50" height="50" x="10" y="10" className={"fill-accent-butter"}/>
+                    </svg>
+                    Butter
                 </div>
 
                 <div className="flex flex-col gap-4 text-2xl">
