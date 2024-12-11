090d9453d36dc821d81295a5c9489119ddcebf48
Benjamin Bolte <ben@bolte.cc>
2024-12-11 00:39:49 -0800

    robot renderer

--- src/components/robot/robotRenderer.tsx
+++ src/components/robot/robotRenderer.tsx
@@ -2,9 +2,12 @@
 
 import React, { useEffect, useRef } from "react";
 import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import URDFLoader from "urdf-loader";
 
 const URDF_URL = "/cad/gpr-20241204.urdf";
+const SCALE = 4;
+const TRANSLATE_Y = 1.9;
 
 const RobotRenderer: React.FC = () => {
   const mountRef = useRef<HTMLDivElement | null>(null);
@@ -17,9 +20,15 @@ const RobotRenderer: React.FC = () => {
       0.1,
       1000
     );
-    const renderer = new THREE.WebGLRenderer();
-    renderer.setSize(window.innerWidth, window.innerHeight);
+    const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
+    if (mountRef.current) {
+      const { clientWidth, clientHeight } = mountRef.current;
+      camera.aspect = clientWidth / clientHeight;
+      camera.updateProjectionMatrix();
+      renderer.setSize(clientWidth, clientHeight);
+    }
     renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setClearColor(0x000000, 0);
 
     if (mountRef.current) {
       mountRef.current.appendChild(renderer.domElement);
@@ -28,6 +37,26 @@ const RobotRenderer: React.FC = () => {
     const loader = new URDFLoader();
     loader.load(URDF_URL, (robot: THREE.Object3D) => {
       scene.add(robot);
+
+      // Correcting for the robot initial size and position.
+      robot.rotateY(Math.PI / 2);
+      robot.translateY(TRANSLATE_Y);
+      robot.scale.set(SCALE, SCALE, SCALE);
+
+      const controls = new OrbitControls(camera, renderer.domElement);
+      controls.enableDamping = true;
+      controls.dampingFactor = 0.25;
+      controls.screenSpacePanning = false;
+      controls.maxPolarAngle = Math.PI / 2;
+
+      const animate = () => {
+        requestAnimationFrame(animate);
+        controls.update();
+        robot.rotation.y += 0.005;
+        renderer.render(scene, camera);
+      };
+
+      animate();
     });
 
     const light = new THREE.AmbientLight(0x404040);
@@ -39,17 +68,13 @@ const RobotRenderer: React.FC = () => {
 
     camera.position.z = 5;
 
-    const animate = () => {
-      requestAnimationFrame(animate);
-      renderer.render(scene, camera);
-    };
-
-    animate();
-
     const handleResize = () => {
-      camera.aspect = window.innerWidth / window.innerHeight;
-      camera.updateProjectionMatrix();
-      renderer.setSize(window.innerWidth, window.innerHeight);
+      if (mountRef.current) {
+        const { clientWidth, clientHeight } = mountRef.current;
+        camera.aspect = clientWidth / clientHeight;
+        camera.updateProjectionMatrix();
+        renderer.setSize(clientWidth, clientHeight);
+      }
     };
 
     window.addEventListener("resize", handleResize);
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,11 @@ const HeaderSection = () => {
     <section className="pt-16">
       <div className="flex flex-col items-center text-center pt-10 px-4 max-w-full">
         <h1 className="text-5xl mt-16">
-          The world&apos;s first humanoid robot for those who prefer transparency over trust.
+          The world&apos;s first humanoid robot
+          <br />
+          for those who prefer
+          <br />
+          transparency over trust.
         </h1>
         <h2 className="text-3xl mt-8">
           Open-source to protect your privacy, ensure fair pricing, and ensure your contributions
@@ -21,7 +25,7 @@ const HeaderSection = () => {
 
 const RobotSection = () => {
   return (
-    <div className="flex flex-col items-center text-center pt-10 max-w-full aspect-square border-2 rounded-lg my-8">
+    <div className="flex flex-col items-center text-center pt-10 max-w-full aspect-square my-8">
       <RobotRenderer />
     </div>
   );
