edf84007ebf2fdf13e63ec62ba9e4fac65c399a1
Benjamin Bolte <ben@bolte.cc>
2024-12-11 01:32:24 -0800

    meshes

--- src/components/robot/robotRenderer.tsx
+++ src/components/robot/robotRenderer.tsx
@@ -7,38 +7,40 @@ import URDFLoader from "urdf-loader";
 import { URDFJoint } from "urdf-loader/src/URDFClasses";
 
 const URDF_URL = "/cad/gpr-20241204.urdf";
-const SCALE = 4;
-const TRANSLATE_Y = 1.9;
+const SCALE = 3;
+const TRANSLATE_Y = 1.5;
 
 interface Waypoint {
   start: number;
   end: number;
 }
 
+// TODO: Instead of doing a sinusoidal pattern, we should provide a sequence
+// of waypoints that the robot will follow.
 const WAYPOINTS: { [key: string]: Waypoint } = {
-  L_shoulder_y: { start: 0, end: 0.5 },
-  L_shoulder_x: { start: 0, end: 0.5 },
-  L_shoulder_z: { start: 0, end: 0.5 },
-  R_elbow: { start: 0, end: 0.5 },
-  R_wrist: { start: 0, end: 0.5 },
-  R_shoulder_y: { start: 0, end: 0.5 },
-  R_shoulder_x: { start: 0, end: 0.5 },
-  R_shoulder_z: { start: 0, end: 0.5 },
-  L_elbow: { start: 0, end: 0.5 },
-  L_wrist: { start: 0, end: 0.5 },
-  L_hip_y: { start: 0, end: 0.5 },
-  L_hip_x: { start: 0, end: 0.5 },
-  L_hip_z: { start: 0, end: 0.5 },
-  L_knee: { start: 0, end: 0.5 },
-  L_ankle: { start: 0, end: 0.5 },
-  R_hip_y: { start: 0, end: 0.5 },
-  R_hip_x: { start: 0, end: 0.5 },
-  R_hip_z: { start: 0, end: 0.5 },
-  R_knee: { start: 0, end: 0.5 },
-  R_ankle: { start: 0, end: 0.5 },
+  L_shoulder_y: { start: Math.PI / 4, end: 0 },
+  L_shoulder_x: { start: 0, end: 0 },
+  L_shoulder_z: { start: Math.PI, end: Math.PI },
+  R_elbow: { start: 0, end: Math.PI / 2 },
+  R_wrist: { start: 0, end: 0.2 },
+  R_shoulder_y: { start: 0, end: -Math.PI / 4 },
+  R_shoulder_x: { start: 0, end: 0 },
+  R_shoulder_z: { start: Math.PI, end: Math.PI },
+  L_elbow: { start: Math.PI / 2, end: 0 },
+  L_wrist: { start: 0, end: 0.2 },
+  L_hip_y: { start: -Math.PI / 4, end: Math.PI / 4 },
+  L_hip_x: { start: 0, end: 0 },
+  L_hip_z: { start: 0, end: 0 },
+  L_knee: { start: -Math.PI / 2, end: 0 },
+  L_ankle: { start: -0.1, end: 0.1 },
+  R_hip_y: { start: -Math.PI / 4, end: Math.PI / 4 },
+  R_hip_x: { start: 0, end: 0 },
+  R_hip_z: { start: 0, end: 0 },
+  R_knee: { start: 0, end: -Math.PI / 2 },
+  R_ankle: { start: -0.1, end: 0.1 },
 };
 
-const DURATION_S = 1;
+const DURATION_S = 5;
 
 const RobotRenderer: React.FC = () => {
   const mountRef = useRef<HTMLDivElement | null>(null);
@@ -79,21 +81,23 @@ const RobotRenderer: React.FC = () => {
       controls.dampingFactor = 0.25;
       controls.screenSpacePanning = false;
       controls.maxPolarAngle = Math.PI / 2;
+      controls.enableZoom = false;
 
-      let time = 0;
+      const startTime = Date.now();
 
       const animate = () => {
         requestAnimationFrame(animate);
         controls.update();
 
         // Update joint positions with a sinusoidal pattern
-        time += 0.01;
+        const time = (Date.now() - startTime) / 1000;
         robot.traverse((child) => {
           const joint = child as URDFJoint;
           if (joint.isURDFJoint) {
             if (WAYPOINTS[joint.name]) {
               const { start, end } = WAYPOINTS[joint.name];
-              const value = start + (end - start) * Math.sin((time * Math.PI) / DURATION_S);
+              const value =
+                start + (end - start) * ((Math.sin((time * Math.PI) / DURATION_S) + 1) / 2);
               joint.setJointValue(value);
             }
           }
