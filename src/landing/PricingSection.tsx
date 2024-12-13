7f982901217017deb8f576dd4c5020d216999a3d
nethen <72537484+nethen@users.noreply.github.com>
2024-12-13 02:37:12 -0800

    fix: removed extra padding on pricing

--- src/landing/PricingSection.tsx
+++ src/landing/PricingSection.tsx
@@ -5,7 +5,7 @@ import { isDarkMode } from "@/components/util/isDarkMode";
 const PricingSection = () => {
   const darkMode = isDarkMode();
   return (
-    <section className="col-span-full grid md:grid-cols-2 3xl:col-span-10 3xl:col-start-2 4xl:col-span-8 4xl:col-start-3 sm:gap-x-[2.5vw] 2xl:gap-x-[1.25vw] gap-y-4 py-16 alignment">
+    <section className="col-span-full grid md:grid-cols-2 3xl:col-span-10 3xl:col-start-2 4xl:col-span-8 4xl:col-start-3 sm:gap-x-[2.5vw] 2xl:gap-x-[1.25vw] gap-y-4 py-16">
       <article className="flex flex-col gap-y-4 p-8 bg-gradient-to-br from-methyl to-rust rounded-lg text-filament">
         <hgroup>
           <span className="text-caption opacity-60">Limited edition &mdash; 20 units total</span>
