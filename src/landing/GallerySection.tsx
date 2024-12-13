95abc51dd8b25f0aa2a67cb874cb114ab85c484a
nethen <72537484+nethen@users.noreply.github.com>
2024-12-13 07:18:15 -0800

    feat: base for sticky photos

--- src/landing/GallerySection.tsx
+++ src/landing/GallerySection.tsx
@@ -1,6 +1,39 @@
+import { motion, useTransform, useScroll } from "motion/react";
+import { useRef } from "react";
+
 const GallerySection = () => {
+  const scrollRef = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: scrollRef,
+    offset: ["start start", "end start"],
+  });
+
   return (
-    <section className="col-span-full bg-gradient-to-b from-methyl via-oxide to-rust min-h-[270svh]"></section>
+    <section
+      className="relative col-span-full bg-gradient-to-b from-methyl via-oxide to-rust min-h-[360dvh]"
+      ref={scrollRef}
+    >
+      <motion.div
+        className="sticky top-16 bg-sol size-8 rounded-full"
+        style={{ opacity: useTransform(scrollYProgress, [0, 0.25], [1, 0]) }}
+      />
+      <motion.div
+        className="sticky top-16 bg-sol size-8 rounded-full translate-x-[100%]"
+        style={{ opacity: useTransform(scrollYProgress, [0.25, 0.5], [1, 0]) }}
+      />
+      <motion.div
+        className="sticky top-16 bg-sol size-8 rounded-full translate-x-[200%]"
+        style={{ opacity: useTransform(scrollYProgress, [0.5, 0.75], [1, 0]) }}
+      />
+      <motion.div
+        className="sticky top-16 bg-sol size-8 rounded-full translate-x-[300%]"
+        style={{ opacity: useTransform(scrollYProgress, [0.75, 1], [1, 0]) }}
+      />
+      <motion.div
+        className="sticky top-16 bg-sol size-8 rounded-full translate-x-[400%]"
+        style={{ opacity: useTransform(scrollYProgress, [0.75, 1], [1, 0]) }}
+      />
+    </section>
   );
 };
 
