e86365975ae4bfb0542f3e9b79e1b69f4d4845db
nethen <72537484+nethen@users.noreply.github.com>
2024-12-14 06:55:20 -0800

    fix: set header image to priority load

--- src/landing/HeaderSection.tsx
+++ src/landing/HeaderSection.tsx
@@ -11,6 +11,7 @@ const HeaderSection = () => {
           alt="Header"
           fill
           className="object-cover brightness-50"
+          priority={true}
         />
       </div>
       <hgroup className="z-10 text-filament col-span-full sm:col-span-4 md:col-span-5 md:col-start-5 lg:col-span-4 lg:col-start-6 xl:col-span-3 xl:col-start-6 2xl:col-start-8 2xl:col-span-5 4xl:col-start-8 4xl:col-span-4 flex flex-col mb-12 gap-4">
