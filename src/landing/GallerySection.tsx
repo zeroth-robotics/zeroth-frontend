3ac93ee10ffe6e87bc2e8518f3b7cbad53174cfc
nethen <72537484+nethen@users.noreply.github.com>
2024-12-14 01:44:19 -0800

    fix: gallery fix

--- src/landing/GallerySection.tsx
+++ src/landing/GallerySection.tsx
@@ -1,4 +1,6 @@
-import { motion, useTransform, useScroll } from "motion/react";
+import { motion, useTransform, useScroll, MotionValue } from "motion/react";
+import { useContext } from "react";
+import { createContext } from "react";
 import { useRef } from "react";
 
 const Images = [
@@ -20,6 +22,21 @@ const Images = [
   },
 ];
 
+const GalleryItem = ({ src, alt, index }: { src: string; alt: string; index: number }) => {
+  const scrollYProgress = useContext(ScrollContext);
+  return (
+    <motion.div
+      className="sticky top-24 h-[60svh] min-h-[16rem] bg-gradient-to-b from-methyl via-oxide to-rust rounded-lg"
+      style={{
+        opacity: useTransform(scrollYProgress, [index * 0.25, (index + 1) * 0.25], [1, 0]),
+        scaleX: useTransform(scrollYProgress, [index * 0.25, (index + 1) * 0.25], [1, 0.8]),
+      }}
+    />
+  );
+};
+
+const ScrollContext = createContext(new MotionValue());
+
 const GallerySection = () => {
   const scrollRef = useRef(null);
   const { scrollYProgress } = useScroll({
@@ -32,16 +49,16 @@ const GallerySection = () => {
       className="relative col-span-full lg:col-span-5 lg:col-start-3 2xl:col-span-8 2xl:col-start-3 flex flex-col gap-y-32"
       ref={scrollRef}
     >
-      {Images.map((image, index) => (
-        <motion.div
-          className="sticky top-24 h-[60svh] min-h-[16rem] bg-gradient-to-b from-methyl via-oxide to-rust rounded-lg"
-          key={`gallery-image-${index}`}
-          style={{
-            opacity: useTransform(scrollYProgress, [index * 0.25, (index + 1) * 0.25], [1, 0]),
-            scaleX: useTransform(scrollYProgress, [index * 0.25, (index + 1) * 0.25], [1, 0.8]),
-          }}
-        />
-      ))}
+      <ScrollContext.Provider value={scrollYProgress}>
+        {Images.map((image, index) => (
+          <GalleryItem
+            key={`gallery-image-${index}`}
+            src={image.src}
+            alt={image.alt}
+            index={index}
+          />
+        ))}
+      </ScrollContext.Provider>
     </section>
   );
 };
