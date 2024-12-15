b7a66955688f684727e06f79434f3890c4b976d6
nethen <72537484+nethen@users.noreply.github.com>
2024-12-14 18:28:27 -0800

    fix: header section copy

--- src/landing/HeaderSection.tsx
+++ src/landing/HeaderSection.tsx
@@ -19,8 +19,7 @@ const HeaderSection = () => {
       <hgroup className="z-10 text-filament col-span-full sm:col-span-4 md:col-span-5 md:col-start-5 lg:col-span-4 lg:col-start-6 xl:col-span-3 xl:col-start-6 2xl:col-start-8 2xl:col-span-5 4xl:col-start-8 4xl:col-span-4 flex flex-col mb-12 gap-4">
         <h1 className="text-heading-lg">Made to be modified</h1>
         <h2 className="text-body">
-          K-Bot is an open-source &amp; auditable robot, for those who prefer transparency over
-          trust.
+          K-Bot is an open-source humanoid robot, for those who prefer transparency over trust.
         </h2>
         <CTASubtitleButton subtitle="Shipping Q1 2025" className="w-full" mode={FillMode.FILL}>
           Preorder K-Bot today
